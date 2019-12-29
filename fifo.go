818df57cfc2d56c451d8302e7571bf6d84b7eab7
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-29 13:42:10 -0500

    adding in statistics channel

--- fifo.go
+++ fifo.go
@@ -11,6 +11,7 @@ import (
 )
 
 type fifo struct {
+	stats  chan<- []string
 	data   *list.List
 	mutex  sync.Mutex
 	closed bool
@@ -18,8 +19,9 @@ type fifo struct {
 }
 
 // New is what reservoird to create a queue
-func New(cfg string) (icd.Queue, error) {
+func New(cfg string, stats chan<- []string) (icd.Queue, error) {
 	o := &fifo{
+		stats:  stats,
 		data:   list.New(),
 		closed: false,
 		Tag:    "fifo",
